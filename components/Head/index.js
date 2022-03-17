0a2e598bb786308a1af17b79ecdb0d12352a3864
Muhammad Randi Pratama <muhammad.pratama@btpn.com>
2022-03-17 23:07:27 +0700

    updating website description

--- components/Head/index.js
+++ components/Head/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function Header ({ title = 'Software Development Blog' }) {
   return <Head>
     <title>{title}</title>
-    <meta name="description" content="Development tutorials step by step." />
+    <meta name="description" content="Tutorials for web development, devops tools, linux and more." />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 }
